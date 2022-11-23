305e61cfde572236c98a4ee487ebaddc053dffac
MDiMicelli <mdimicelli@northwell.edu>
2022-11-23 08:06:29 -0500

    finish 3.4

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const persons = [
+let persons = [
   { 
     "name": "Arto Hellas", 
     "number": "040-123456",
@@ -34,6 +34,17 @@ app.get('/api/persons/:id', (req, res) => {
   const person = persons.find(p => p.id === id);
   if (person) {
     res.json(person);
+  }
+  else {
+    res.status(404).end();
+  }
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  if (persons.find(p => p.id === id)) {
+    persons = persons.filter(p => p.id !== id);
+    res.status(204).end();
   }
   else {
     res.status(404).end();
