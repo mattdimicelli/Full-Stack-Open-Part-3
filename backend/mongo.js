8590aa77927542fcf32a25333d3380081af17f5d
Matthew Di Micelli <matthewdimicelli@gmail.com>
2022-11-27 19:59:02 -0500

    finish 3.12

--- backend/mongo.js
+++ backend/mongo.js
@@ -1,10 +1,5 @@
 const mongoose = require('mongoose');
 
-if (process.argv.length < 3) {
-    console.log('Please provide the password as an argument: node mongo.js <password>');
-    process.exit(1);
-}
-
 const password = encodeURIComponent(process.argv[2]);
 
 const url = `mongodb+srv://mrd2689a_full_stack_open:${password}@cluster0.2rdjo2h.mongodb.net/phone-book?retryWrites=true&w=majority`;
@@ -14,10 +9,31 @@ mongoose.connect(url);
 const entrySchema = new mongoose.Schema({
     name: String,
     number: String,
-    important: Boolean,
 });
 
 const Entry = mongoose.model('Entry', entrySchema);
+
+if (process.argv.length === 3) { // if there is only one argument passed
+    Entry.find({}).then(result => {
+        result.forEach(entry => console.log(`${entry.name}: ${entry.number}`));
+        mongoose.connection.close();
+        process.exit(1);
+    });
+}
+else if (process.argv.length === 5) {
+    const entry = new Entry({
+        name: process.argv[3],
+        number: process.argv[4],
+    });
+    
+    entry.save().then(result => {
+        console.log(`Added ${entry.name} with number ${entry.number} to phonebook`);
+        mongoose.connection.close();
+    });
+}
+
+
+
 
 // const note = new Note({
 //     content: 'HTML is Easy',
